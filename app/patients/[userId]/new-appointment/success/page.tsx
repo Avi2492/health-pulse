8ca5151b3bfabcff5fdcd3da650b6b26bc350d01
Avinash Pandey <avinash.12009084@lpu.in>
2024-07-09 14:52:48 +0530

    Success Page

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -1,7 +1,84 @@
+/* eslint-disable react/no-unescaped-entities */
+import { Button } from "@/components/ui/button";
+import { Doctors } from "@/constants";
+import { getAppointmentDetails } from "@/lib/actions/appointment.action";
+import { formatDateTime } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-const SuccessPage = () => {
-  return <div>SuccessPage</div>;
+const SuccessPage = async ({
+  params: { userId },
+  searchParams,
+}: SearchParamProps) => {
+  const appointmentId = (searchParams?.appointmentId as string) || "";
+  const appointment = await getAppointmentDetails(appointmentId);
+
+  const doctor = Doctors.find(
+    (doc) => doc.name === appointment.primaryPhysician
+  );
+
+  return (
+    <div className="flex h-screen max-h-screen px-[5%]">
+      <div className="success-img">
+        <Link href={"/"}>
+          <Image
+            src={"/assets/icons/logo-full.svg"}
+            width={1000}
+            height={1000}
+            alt="success-logo"
+            className="h-10 w-fit"
+          />
+        </Link>
+        <section className="flex flex-col items-center">
+          <Image
+            src={"/assets/gifs/success.gif"}
+            height={300}
+            width={280}
+            alt="success"
+          />
+          <h2 className="header mb6 max-w-[600px] text-center">
+            Your <span className="text-green-500">appointment request</span> has
+            been successfully submitted!
+          </h2>
+          <p>We'll be in touch shortly to continue</p>
+        </section>
+        <section className="request-details">
+          <p>Requested appointment details:</p>
+          <div className="flex items-center gap-3">
+            <Image
+              src={doctor?.image!}
+              width={100}
+              height={100}
+              alt="doctor"
+              className="size-6"
+            />
+            <p className="whitespace-nowrap">Dr. {doctor?.name}</p>
+          </div>
+          <div className="flex gap-2">
+            <Image
+              src={"/assets/icons/calendar.svg"}
+              height={24}
+              width={24}
+              alt="calendar-logo"
+            />
+            <p>{formatDateTime(appointment.schedule).dateTime}</p>
+          </div>
+        </section>
+        <Button variant={"outline"} className="shad-primary-btn" asChild>
+          <Link href={`/patients/${userId}/new-appointment`}>
+            New Appointment
+          </Link>
+        </Button>
+        <div className="flex justify-center items-center text-center">
+          <p className="copyright py-12">
+            All &copy; {new Date().getFullYear()} Reserved by spheri
+            <span className="text-orange-500">soft</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SuccessPage;
